9ba3ae36c9851bd7f5089a1360a4c4e9a593b263
Conrado Jorge <67123333+ConradoJorge@users.noreply.github.com>
2021-01-11 23:03:34 -0500

    Added functionality
    Added the ability for the user to enter their own grades with exception handling, also added letter grades as an output once program completes.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+
 namespace GradeBook
 
 {
@@ -8,15 +9,38 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book ("Conrado's Grade Book");
-            book.AddGrade(90.5);
-            book.AddGrade(89.1);
-            book.AddGrade(77.5);
+
+            while (true){
+                Console.Write("Please Enter a grade between 0.0 and 100.0 or press 'q' to exit. >>: ");
+                var input = Console.ReadLine();
+
+                if (input == "q" || input == "Q"){
+                    break;
+                }
+                try{
+
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+
+                }catch(ArgumentException ex){
+
+                    Console.WriteLine(ex.Message);
+
+                }
+                catch(FormatException ex){
+                    Console.WriteLine(ex.Message);
+                }
+                finally{
+                    //Runs code after exception handling.
+                }
+            }
 
             var stats = book.GetStatistics();
             
             Console.WriteLine($"The lowest grade is {stats.Low:N1}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The highest grade is {stats.High:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 }
